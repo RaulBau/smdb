67aba0001c59694a4d3acb00cb61cb0a8d8c2a93
raulbau <raulb@111btec.com>
2020-12-03 02:03:15 -0600

    Se agregaron los puntos 1,2,3

--- SMBD/Form1.Designer.cs
+++ SMBD/Form1.Designer.cs
@@ -29,13 +29,122 @@ namespace SMBD
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFD1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFD1 = new System.Windows.Forms.OpenFileDialog();
+            this.tablasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFDTabla = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.tablasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.AccessibleName = "Archivo";
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.cerrarToolStripMenuItem,
+            this.nuevaToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            this.archivoToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.archivoToolStripMenuItem_DropDownItemClicked);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.AccessibleName = "Abrir";
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.AccessibleName = "Cerrar";
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            // 
+            // nuevaToolStripMenuItem
+            // 
+            this.nuevaToolStripMenuItem.AccessibleName = "Nueva";
+            this.nuevaToolStripMenuItem.Name = "nuevaToolStripMenuItem";
+            this.nuevaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevaToolStripMenuItem.Text = "Nueva";
+            // 
+            // saveFD1
+            // 
+            this.saveFD1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFD1_FileOk);
+            // 
+            // openFD1
+            // 
+            this.openFD1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFD1_FileOk);
+            // 
+            // tablasToolStripMenuItem
+            // 
+            this.tablasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaToolStripMenuItem1});
+            this.tablasToolStripMenuItem.Name = "tablasToolStripMenuItem";
+            this.tablasToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.tablasToolStripMenuItem.Text = "Tabla";
+            this.tablasToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.tablasToolStripMenuItem_DropDownItemClicked);
+            // 
+            // nuevaToolStripMenuItem1
+            // 
+            this.nuevaToolStripMenuItem1.AccessibleName = "Nueva";
+            this.nuevaToolStripMenuItem1.Name = "nuevaToolStripMenuItem1";
+            this.nuevaToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.nuevaToolStripMenuItem1.Text = "Nueva";
+            // 
+            // saveFDTabla
+            // 
+            this.saveFDTabla.DefaultExt = "t";
+            this.saveFDTabla.Filter = "Tabla|*.t|Todos los archivos|*.*";
+            this.saveFDTabla.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFDTabla_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFD1;
+        private System.Windows.Forms.OpenFileDialog openFD1;
+        private System.Windows.Forms.ToolStripMenuItem tablasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaToolStripMenuItem1;
+        private System.Windows.Forms.SaveFileDialog saveFDTabla;
     }
 }
 
--- SMBD/Form1.cs
+++ SMBD/Form1.cs
@@ -7,14 +7,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace SMBD
 {
     public partial class Form1 : Form
     {
+
+        string pathBase;
+        private FolderBrowserDialog folderBD;
+        CommonOpenFileDialog commonOFD;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pathBase = "";
+            folderBD = new FolderBrowserDialog();
+            folderBD.ShowNewFolderButton = false;
+            commonOFD = new CommonOpenFileDialog();
+            commonOFD.IsFolderPicker = true;
+            inicializaDirectorio(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+        }
+
+        #region Archivo
+
+        private void saveFD1_FileOk(object sender, CancelEventArgs e)
+        {
+            pathBase = saveFD1.FileName;
+            Directory.CreateDirectory(pathBase);
+            //Directory.SetCurrentDirectory(pathBase);
+            inicializaDirectorio(pathBase);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+
+        private void openFD1_FileOk(object sender, CancelEventArgs e)
+        {
+            pathBase = commonOFD.FileName;
+            Directory.SetCurrentDirectory(pathBase);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+
+        private void inicializaDirectorio(string directorio)
+        {
+            commonOFD.InitialDirectory = directorio;
+            openFD1.InitialDirectory = directorio;
+            saveFD1.InitialDirectory = directorio;
+            saveFDTabla.InitialDirectory = directorio;
+            Directory.SetCurrentDirectory(directorio);
+        }
+
+        #endregion
+
+        #region menu
+        private void archivoToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            archivoToolStripMenuItem.DropDown.Close();
+            switch (e.ClickedItem.AccessibleName)
+            {
+                case "Abrir":
+                    if (commonOFD.ShowDialog() == CommonFileDialogResult.Ok)
+                    {
+                        pathBase = commonOFD.FileName;
+                        inicializaDirectorio(pathBase);
+                        Console.WriteLine(Directory.GetCurrentDirectory());
+                    }
+                    break;
+                case "Cerrar":
+                    pathBase = "";
+                    break;
+                case "Nueva":
+                    saveFD1.ShowDialog();
+                    break;
+            }
+        }
+        #endregion
+
+        #region tabla
+
+        private void tablasToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            tablasToolStripMenuItem.DropDown.Close();
+            switch (e.ClickedItem.AccessibleName)
+            {
+                case "Nueva":
+                    if (pathBase != "")
+                    {
+                        saveFDTabla.ShowDialog();
+                    }
+                    break;
+            }
+        }
+
+
+        private void saveFDTabla_FileOk(object sender, CancelEventArgs e)
+        {
+            File.CreateText(saveFDTabla.FileName);
+        }
+        #endregion
     }
 }
